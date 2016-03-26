50cbe45de63a6249bb6b62b7bcb3ec0b1b241ead
JoshMina <jjmoona0@gmail.com>
2016-03-26 15:56:09 +1300

    Connection between Basex server and Website established

--- movieWebsite2/routes/index.js
+++ movieWebsite2/routes/index.js
@@ -1,30 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var cheerio = require ('cheerio')
+//var cheerio = require ('cheerio')
 var basex = require ('basex')
 var client = new basex.Session ("127.0.0.1", 1984, "admin", "admin");
-client.execute ("open movies");
+client.execute ("OPEN Colenso");
 
 /* GET home page. */
-// Testing master update
-router.get('/', function(req, res, next) {
-client.execute("xquery for $movie in //movie[position() lt 10] "+ 
-	"return <movie> {$movie/title} {$movie/id} </movie>",
-function(body) {
+// Testing master update 2
+router.get("/", function(req, res) {
+	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" + 
+	"//name[@type='place' and position() = 1 and . = 'Manawarakau']",
+	function(error,result) {
 	 
-	 var $ = cheerio.load(body);
-	 var list = [];
-	 $('movie').each(function (i,elem) {
-	 	var title = $(elem).find('title').text();
-	 	var id = $(elem).find('id').text();
-	 	var url = 'images/' + id + '.jpg';
-	 	list.push ({p:title, image: url});
-
-	 });
-	  res.render('index', { title: 'ECS Movie Rental' , images : list });
-	}
-	);
- 
+	 //var $ = cheerio.load(body);
+	 //var list = [];
+	 //$('movie').each(function (i,elem) {
+	 	//var title = $(elem).find('title').text();
+	 	//var id = $(elem).find('id').text();
+	 	//var url = 'images/' + id + '.jpg';
+	 	//list.push ({p:title, image: url});
+	 	if (error){ console.error(error);}
+	 	else{
+	 		res.render('index', { title: 'ECS Video Rental' , placer : result.result });
+	 				// varName: req.query.text box name. This is to get the input from the text box
+	 	}
+	 		}
+	 		);
 });
 
-//module.exports = router;
+module.exports = router;
