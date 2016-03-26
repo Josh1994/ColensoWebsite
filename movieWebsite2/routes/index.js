ba5b17f9372f10511f73014b433cd11da956c99d
JoshMina <jjmoona0@gmail.com>
2016-03-26 17:05:28 +1300

    deleted cheerio

--- movieWebsite2/routes/index.js
+++ movieWebsite2/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-//var cheerio = require ('cheerio')
 var basex = require ('basex')
 var client = new basex.Session ("127.0.0.1", 1984, "admin", "admin");
 client.execute ("OPEN Colenso");
